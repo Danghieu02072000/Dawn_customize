47d91332a543dbc40d9bd14e5b8bb784c3f2ecb6
Dang Hieu <danghieutieumai@gmail.com>
2023-09-28 14:59:22 +0700

    done slider in page home

---
+++ assets/main123.js
@@ -0,0 +1,17 @@
+$('.customeContainer').owlCarousel({
+  loop:true,
+  margin:30,
+  nav:true,
+  autoHeight: true,
+  responsive:{
+      0:{
+          items:2
+      },
+      600:{
+          items:3
+      },
+      1024:{
+          items:5
+      }
+  }
+})
