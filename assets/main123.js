04e7f24ae0f0ab443f55e32afee08f7d633690dd
Dang Hieu <danghieutieumai@gmail.com>
2023-10-01 23:48:13 +0700

    responsive mobie

--- assets/main123.js
+++ assets/main123.js
@@ -4,6 +4,16 @@ $('.customeContainer').owlCarousel({
   margin:30,
   nav:true,
   autoHeight: true,
-  items: window.number_columns
+  items: window.number_columns,
+  responsive: {
+    0: {
+      items: 1 // Số lượng item hiển thị khi chiều rộng màn hình nhỏ hơn hoặc bằng 0px
+    },
+    768: {
+      items: 2 // Số lượng item hiển thị khi chiều rộng màn hình từ 768px trở lên
+    },
+    1024: {
+      items: 3 // Số lượng item hiển thị khi chiều rộng màn hình từ 1024px trở lên
+    }
+  }
 });
- console.log(window.number_columns)
